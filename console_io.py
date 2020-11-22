a085df7ae68a6105f9b48f5e8087397022d26371
Lanfear7 <brennan1086@gmail.com>
2020-11-22 02:30:18 -0800

    regex is implemented

--- console_io.py
+++ console_io.py
@@ -1,5 +1,19 @@
-import bookstore
+import bookstore, re
 
 def check_data(file_name, new_title, new_author, new_year, new_isbn, new_description):
     file = file_name[2:-3]
+    regex_title = "/^[a-zA-Z]+[0-9]* /"
+    regex_author ="/^[a-zA-Z]+ /"
+    if len(new_year) < 4 or int(new_year) < 1900:
+        print('ERROR')
+        error = True
+        while error:
+            print ('Input a valid year after 1900')
+            y = input()
+            if len(y) >= 4 and int(y) > 1900:
+                error = False 
+
+
+    regex_isbn = "/^[0-9]{4}$/"
+    regex_description = "/^[a-zA-Z]{0,256}/"
     print('working')
